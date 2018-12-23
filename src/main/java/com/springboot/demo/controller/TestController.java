72b3cf9dcbd7d9e8aa12f3c05a10624fb03ca2ea
huangzd <314811607@qq.com>
2018-12-23 22:37:47 +0800

    新增controller测试

---
+++ src/main/java/com/springboot/demo/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.springboot.demo.controller;
+
+/**
+ * @author:huangzd
+ * @date:2018/12/23 22:24
+ */
+public class TestController {
+    public static void main(String[] args) {
+         
+    }
+}
